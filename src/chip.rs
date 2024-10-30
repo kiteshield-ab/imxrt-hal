e0817dca8a6cf64cdda633ffd41183d1fdec212a
Nils Fitinghoff <nils.fitinghoff@kiteshield.com>
2024-10-30 15:58:13 +0100

    imxrt1180 gpio

--- src/chip.rs
+++ src/chip.rs
@@ -2,14 +2,5 @@
 //!
 //! These submodules may vary by chip family.
 
-cfg_if::cfg_if! {
-    if #[cfg(family = "imxrt10xx")] {
-        mod imxrt10xx;
-        pub use imxrt10xx::*;
-    } else if #[cfg(family = "imxrt11xx")] {
-        mod imxrt11xx;
-        pub use imxrt11xx::*;
-    } else {
-        mod none; pub use none::*;
-    }
-}
+mod imxrt1180;
+pub use imxrt1180::*;
---
+++ src/chip/imxrt1180.rs
@@ -0,0 +1,24 @@
+pub mod gpio;
+pub mod ccm {}
+pub mod dma {}
+
+pub(crate) mod reexports {
+    pub use super::gpio;
+}
+
+pub(crate) mod iomuxc {
+    pub use super::config::pads;
+    use crate::ral;
+
+    /// Transform the `imxrt-ral` IOMUXC instances into pad objects.
+    pub fn into_pads(_: ral::iomuxc::IOMUXC, _: ral::iomuxc_aon::IOMUXC_AON) -> pads::Pads {
+        // Safety: acquiring pads has the same safety implications
+        // as acquiring the IOMUXC instances. The user has already
+        // assumed the unsafety.
+        unsafe { pads::Pads::new() }
+    }
+}
+
+mod config {
+    pub use imxrt_iomuxc::imxrt1180 as pads;
+}
